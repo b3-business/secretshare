f445b9815c2a41530b3286dcc691665d99e5f736
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-11 00:14:57 +0200

    remove useless curly braces

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ import Log from "../islands/Log.tsx";
 
 export default function Home() {
   return (
-    <main class={"flex flex-col items-center justify-center"}>
+    <main class="flex flex-col items-center justify-center">
       <CreateSecret />
       <Log />
     </main>
