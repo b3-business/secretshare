49690ae137e22ee67f16c7cccd35ade05793204e
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-12 19:58:57 +0200

    improve styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <div class="px-2 py-4 mx-auto bg-primary">
+    <div class="px-2 py-4 mx-auto bg-primary ">
       <a href="/">
-        <header class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <header class="md:max-w-screen mx-auto flex flex-col items-center justify-center">
           <h1 class="text-4xl font-bold">SecretShare</h1>
           <p class="text-3xl">
             Teile Passwörter und Tokens sicher über Einmal-View URL
