951aa1128256cab78ae22677a59e6075e9ba5ff8
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-16 14:50:45 +0200

    improve basic layout of fetchSecret

--- routes/secret/[uuid].tsx
+++ routes/secret/[uuid].tsx
@@ -6,12 +6,16 @@ export default function UUID(props: PageProps) {
   const { uuid } = props.params;
   const encryptionKey = atob(props.url.searchParams.get("encryptionKey") || "");
   return (
-    <main class="flex flex-col items-center justify-center">
-      <h2>Secret abrufen</h2>
-      <p>UUID: {uuid}</p>
-      <p>
+    <main class="flex flex-col items-center justify-center w-full max-w-[90%] md:max-w-2xl gap-4 mx-auto">
+      <h2 class="mt-6 text-2xl font-bold items-center flex flex-col">
+        Secret abrufen
+      </h2>
+      <p class="font-bold wrap-anywhere text-center">UUID: {uuid}</p>
+      <p class="text-balance text-center">
         Mit dem Klick auf den nachfolgenden Button wird das Secret abgerufen.
-        Danach ist dieses <strong>nicht</strong> mehr abrufbar.
+        Danach ist dieses{" "}
+        <strong class="underline underline-offset-2">nicht</strong>{" "}
+        mehr abrufbar!
       </p>
       <FetchSecret uuid={uuid} encryptionKey={encryptionKey} />
       <Log />
