0fd0326f63549693d4ca261d80370cbb5b844d61
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-16 14:37:49 +0200

    improve flexbox design on createSecret island

--- islands/CreateSecret.tsx
+++ islands/CreateSecret.tsx
@@ -5,14 +5,14 @@ import ViewCountLimitInput from "../components/CreateSecret/ViewCountLimitInput.
 
 export default function CreateSecret() {
   return (
-    <section>
+    <section class="w-full max-w-[90%] md:max-w-2xl">
       <h2 class="my-4 text-2xl font-bold items-center flex flex-col">
         Erstelle ein neues Secret
       </h2>
       <form
         name="createSecret"
         method="post"
-        class="flex flex-col items-center"
+        class="flex flex-col items-stretch gap-4"
         onSubmit={onSubmit}
         autocomplete="off"
       >
@@ -24,17 +24,19 @@ export default function CreateSecret() {
         />
         <textarea
           name="secret"
-          class="w-[90%] min-h-40 p-2 border-2 border-gray-500 rounded"
+          class="min-h-40 md:min-h-60 p-2 border-2 border-gray-500 rounded"
           placeholder="Passwort, Token oder andere Information..."
         />
 
-        <div class="grid grid-cols-[min-content_1fr] gap-4 items-center justify-center w-[90%] mt-4">
+        <div class="grid grid-cols-[min-content_1fr] gap-4 items-center">
           <CustomExpireInForm />
           <PassphraseInput />
           <ViewCountLimitInput />
         </div>
 
-        <button class="p-2 self-stretch" type="submit">Anlegen</button>
+        <button class="p-2" type="submit">
+          Anlegen
+        </button>
       </form>
     </section>
   );
