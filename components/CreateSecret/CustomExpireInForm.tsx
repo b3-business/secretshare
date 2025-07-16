d9432720a241c6aeb7bc410ae629aae3056ffa48
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-16 14:13:11 +0200

    fix layout in custom expire in form

--- components/CreateSecret/CustomExpireInForm.tsx
+++ components/CreateSecret/CustomExpireInForm.tsx
@@ -26,30 +26,34 @@ export default function CustomExpireInForm() {
           <div>
             {/* placeholder div to keep the first grid column empty */}
           </div>
-          <form id="duration">
-            <input
-              class="p-2 border-2 border-gray-500 rounded"
-              id="h"
-              name="h"
-              type="number"
-              min="0"
-              max="999"
-              value={0}
-              onWheel={scrollNumberInputValue}
-            />
-            <label for="h">h</label>
-            &nbsp;
-            <input
-              class="p-2 border-2 border-gray-500 rounded"
-              id="m"
-              name="m"
-              type="number"
-              min="0"
-              max="59"
-              value={0}
-              onWheel={scrollNumberInputValue}
-            />
-            <label for="m">m</label>
+          <form id="duration" class="flex flex-row gap-2 justify-center">
+            <label for="h">
+              <input
+                class="p-2 border-2 border-gray-500 rounded"
+                id="h"
+                name="h"
+                type="number"
+                min="0"
+                max="999"
+                value={0}
+                onWheel={scrollNumberInputValue}
+              />
+              &nbsp;h
+            </label>
+
+            <label for="m">
+              <input
+                class="p-2 border-2 border-gray-500 rounded"
+                id="m"
+                name="m"
+                type="number"
+                min="0"
+                max="59"
+                value={0}
+                onWheel={scrollNumberInputValue}
+              />
+              &nbsp;m
+            </label>
           </form>
         </>
       )}
